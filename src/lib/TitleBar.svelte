8761eaf02b9cc421f65544d3bcbb67f6342be25d
paulf <paul.frischeisen@web.de>
2023-06-23 18:08:16 +0200

    Setup basic routes and pages for Login/Signup

---
+++ src/lib/TitleBar.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title = '';
+    export let subTitle = '';
+</script>
+
+<div class="box has-text-centered columns m-2">
+    <div class="column">
+        <div class="title is-5">{title}</div>
+        <div class="subtitle is-5">{subTitle}</div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from '$lib/Header.svelte';
+    import WelcomeNavigator from "$lib/WelcomeNavigator.svelte";
+</script>
+
+<Header>
+    <WelcomeNavigator />
+</Header>
+
+<div class="columns is-vcentered content">
+    <div class="column has-text-centered">
+        <img width="300" src="/boulder.png" alt="homer" />
+    </div>
+    <div class="column">
+        <h1 class="title">Discover and Share Climbing Spots in Regensburg</h1>
+        <p>Add whatever place you like and describe what you can do there!</p>
+    </div>
+</div>
